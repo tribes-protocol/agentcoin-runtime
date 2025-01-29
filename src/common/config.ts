5419345eb52e9e817c40393c3b170a4c9cd6c68e
Aditya Veer Parmar <nakulparmar15@gmail.com>
2025-01-29 13:59:14 +0530

    minor

--- src/common/config.ts
+++ src/common/config.ts
@@ -67,5 +67,15 @@ export function getTokenForProvider(
       return undefined
     case ModelProviderName.LIVEPEER:
       return undefined
+    case ModelProviderName.MISTRAL:
+      return undefined
+    case ModelProviderName.NINETEEN_AI:
+      return undefined
+    case ModelProviderName.LETZAI:
+      return undefined
+    case ModelProviderName.DEEPSEEK:
+      return undefined
+    case ModelProviderName.INFERA:
+      return undefined
   }
 }
