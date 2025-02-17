4bc650474aabf920a8d7e3a8b388e8ff4c91e299
Hish Bouabdallah <hishboy@gmail.com>
2025-02-17 15:05:08 -0800

    log home directory

--- src/index.ts
+++ src/index.ts
@@ -36,6 +36,7 @@ export function createAgent(
   walletService: IWalletService
 ): AgentcoinRuntime {
   elizaLogger.success(elizaLogger.successesTitle, 'Creating runtime for character', character.name)
+  elizaLogger.log('Home directory:', process.env.HOME)
 
   const nodePlugin = createNodePlugin()
 
